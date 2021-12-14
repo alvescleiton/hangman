737910fb54b357e634b6610ffc141ab7082655d2
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-12-13 21:47:20 -0300

    Both margin and person position fix

--- src/components/Gibbet/styles.ts
+++ src/components/Gibbet/styles.ts
@@ -4,7 +4,8 @@ export const ImgBackground = styled.div`
   position: relative;
   background-image: url(/assets/img/gibbet.png);
   background-repeat: no-repeat;
-  background-position: 35% top;
+  background-position: center top;
   width: 100%;
   height: 260px;
+  margin-bottom: 30px;
 `
