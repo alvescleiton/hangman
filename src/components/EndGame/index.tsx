7665920b63c289b1a9d70003323bf2610cd6dfff
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-12-17 17:54:09 -0300

    Winner and loser component

--- src/components/EndGame/index.tsx
+++ src/components/EndGame/index.tsx
@@ -1,19 +1,29 @@
 import { useContext } from "react"
 import { EndGameContext } from "../../hooks/EndGameContext"
-import { Container } from "./styles"
+import { WordContext } from "../../hooks/WordContext"
+import { Container, WinnerContainer, Title, Subtitle, LoserContainer, ResetButton } from "./styles"
 
 const EndGame = () => {
   const { endGame, winner, loser, resetGame } = useContext(EndGameContext)
+  const { word } = useContext(WordContext)
 
   return (
     <Container endGame={endGame}>
-      {winner || loser ? (
-        <>
-          You {winner ? 'win!!! =)' : 'lose! =('}
-          <br />
-          <button onClick={resetGame}>Reset</button>
-        </>
-      ) : null}
+      {winner && (
+        <WinnerContainer>
+          <Title>You win!!! =)</Title>
+          <Subtitle>The word is <strong>{word ? word : '...'}</strong></Subtitle>
+        </WinnerContainer>
+      )}
+
+      {loser && (
+        <LoserContainer>
+          <Title>You lose! =(</Title>
+          <Subtitle>The word is <strong>{word ? word : '...'}</strong></Subtitle>
+        </LoserContainer>
+      )}
+
+      <ResetButton onClick={resetGame}>Restart Game</ResetButton>
     </Container>
   )
 }
--- src/components/EndGame/styles.ts
+++ src/components/EndGame/styles.ts
@@ -12,6 +12,7 @@ export const Container = styled.div<ContainerProps>`
   height: 100%;
   z-index: 999;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   pointer-events: none;
@@ -25,3 +26,52 @@ export const Container = styled.div<ContainerProps>`
     backdrop-filter: blur(5px);
   `}
 `;
+
+export const Title = styled.div`
+  font-size: 34px;
+  line-height: 34px;
+  margin: 20px 0;
+`
+
+export const Subtitle = styled.div`
+  font-size: 20px;
+`
+
+export const WinnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  ${Title} {
+    color: #00d01b;
+  }
+`
+
+export const LoserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  ${Title} {
+    color: #ff0000;
+  }
+`
+
+export const ResetButton = styled.button`
+  margin-top: 50px;
+  background: #333;
+  border: 2px solid #333;
+  color: #FFF;
+  font-size: 16px;
+  padding: 8px 20px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: .2s all ease;
+
+  &:hover {
+    background: #FFF;
+    color: #333;
+  }
+`
